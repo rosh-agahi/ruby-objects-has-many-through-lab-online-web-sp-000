eaed2e1f446adf5608591950e70e1b2903988adc
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 06:35:06 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,26 +1,16 @@
-class Doctor
-  attr_accessor :name
+class Appointment
+  attr_accessor :date, :doctor, :patient
 
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
     @@all << self
   end
 
   def self.all
     @@all
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
-  end
-
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
-
-  def patients
-    appointments.map(&:patient)
   end
 end
